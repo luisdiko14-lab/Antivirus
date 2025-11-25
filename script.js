46390820ae579fda3f1974f3df147b6da8c1a721
luisdiko14-lab <luisdiko14@gmail.com>
2025-11-25 12:32:45 +0000

    Add Lorry Anti-Virus installer page with HTML, CSS, and JS

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById("installBtn").addEventListener("click", () => {
+    window.location.href = 
+        "https://github.com/luisdiko14-lab/Antivirus/blob/main/Lorry%20Anti%20Virus.exe";
+});
